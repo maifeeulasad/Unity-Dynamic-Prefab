310ea613ee087da817b5655e6ba9efd3a9c6efcb
maifeeulasad <maifeeulasad@gmail.com>
2019-04-15 17:42:20 +0600

    dynamic prefab added - basic
    inspired from here : https://stackoverflow.com/questions/55674628/how-i-use-duplicate-scene-in-unity-with-different-roads-or-platform-color-with-s/

---
+++ Unity Dynamic Prefab/Assets/Scripts/MaterialController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MaterialController : MonoBehaviour
+{
+    public Material[] materials = new Material[2];
+
+    void Awake()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName.Equals("RedScene"))
+        {
+            GetComponent<MeshRenderer>().material = materials[0];
+        }
+        else if (sceneName.Equals("GreenScene"))
+        {
+            GetComponent<MeshRenderer>().material = materials[1];
+        }
+    }
+    
+}
